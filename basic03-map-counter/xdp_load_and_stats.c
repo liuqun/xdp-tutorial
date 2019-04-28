dba622dddc4900dc195f0fe3d3ee3707b6f41993
Liu Qun <517067180@qq.com>
2019-04-27 19:41:37 -0700

    basic03 homework assiment 1: Add bytes counter

--- basic03-map-counter/xdp_load_and_stats.c
+++ basic03-map-counter/xdp_load_and_stats.c
@@ -122,7 +122,7 @@ static void stats_print(struct stats_record *stats_rec,
 
 	/* Assignment#2: Print other XDP actions stats  */
 	{
-		char *fmt = "%-12s %'11lld pkts (%'10.0f pps)"
+		char *fmt = "%-12s %'11lld pkts %'11lld bytes (%'10.0f pps)"
 			//" %'11lld Kbytes (%'6.0f Mbits/s)"
 			" period:%f\n";
 		const char *action = action2str(XDP_PASS);
@@ -136,7 +136,7 @@ static void stats_print(struct stats_record *stats_rec,
 		packets = rec->total.rx_packets - prev->total.rx_packets;
 		pps     = packets / period;
 
-		printf(fmt, action, rec->total.rx_packets, pps, period);
+		printf(fmt, action, rec->total.rx_packets, rec->total.rx_bytes, pps, period);
 	}
 }
 
@@ -181,6 +181,7 @@ static bool map_collect(int fd, __u32 map_type, __u32 key, struct record *rec)
 
 	/* Assignment#1: Add byte counters */
 	rec->total.rx_packets = value.rx_packets;
+	rec->total.rx_bytes = value.rx_bytes;
 	return true;
 }
 
--- basic03-map-counter/xdp_prog_kern.c
+++ basic03-map-counter/xdp_prog_kern.c
@@ -27,6 +27,8 @@ int  xdp_stats1_func(struct xdp_md *ctx)
 {
 	// void *data_end = (void *)(long)ctx->data_end;
 	// void *data     = (void *)(long)ctx->data;
+	// __u64 bytes = data_end - data;
+	__u64 bytes = (__u64)(ctx->data_end - ctx->data);
 	struct datarec *rec;
 	__u32 key = XDP_PASS; /* XDP_PASS = 2 */
 
@@ -43,12 +45,13 @@ int  xdp_stats1_func(struct xdp_md *ctx)
 	 * use an atomic operation.
 	 */
 	lock_xadd(&rec->rx_packets, 1);
-        /* Assignment#1: Add byte counters
-         * - Hint look at struct xdp_md *ctx (copied below)
-         *
-         * Assignment#3: Avoid the atomic operation
-         * - Hint there is a map type named BPF_MAP_TYPE_PERCPU_ARRAY
-         */
+	/* Assignment#1: Add byte counters
+	 * - Hint look at struct xdp_md *ctx (copied below)
+	 */
+	lock_xadd(&rec->rx_bytes, bytes);
+	/* Assignment#3: Avoid the atomic operation
+	 * - Hint there is a map type named BPF_MAP_TYPE_PERCPU_ARRAY
+	 */
 
 	return XDP_PASS;
 }
